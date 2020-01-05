f4af8a89a02ead47aa18b35dab73a564f033ac9f
pushkin-82 <sputylin@gmail.com>
2020-01-05 19:30:59 +0200

    [HW_01] add concurrencyMultiply

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,8 @@
 package ru.javaops.masterjava.matrix;
 
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
 
 /**
  * gkislin
@@ -10,29 +10,58 @@ import java.util.concurrent.ExecutorService;
  */
 public class MatrixUtil {
 
-    // TODO implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        executor.submit(
+                () -> IntStream.range(0, matrixSize)
+                        .parallel()
+                        .forEach(i -> {
+                            final int[] rowA = matrixA[i];
+                            final int[] rowC = matrixC[i];
+
+                            for (int idx = 0; idx < matrixSize; idx++) {
+                                final int elA = rowA[idx];
+                                final int[] rowB = matrixB[idx];
+                                for (int col = 0; col < matrixSize; col++) {
+                                    rowC[col] += elA * rowB[col];
+                                }
+                            }
+                        })).get();
+
+//        executor.submit(() -> IntStream.range(0, matrixSize)
+//                        .parallel()
+//                        .forEach(j -> multiplyMatrices(matrixA, matrixB, matrixSize, matrixC, j))).get();
         return matrixC;
     }
 
-    // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
-                for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
-                }
-                matrixC[i][j] = sum;
-            }
+        for (int j = 0; j < matrixSize; j++) {
+            multiplyMatrices(matrixA, matrixB, matrixSize, matrixC, j);
         }
+
         return matrixC;
+    }
+
+    private static void multiplyMatrices(int[][] matrixA, int[][] matrixB, int matrixSize, int[][] matrixC, int j) {
+        final int[] columnNeeded = new int[matrixSize];
+        for (int k = 0; k < matrixSize; k++) {
+            columnNeeded[k] = matrixB[k][j];
+        }
+
+        for (int i = 0; i < matrixSize; i++) {
+            int[] rowNeeded = matrixA[i];
+            int sum = 0;
+
+            for (int k = 0; k < matrixSize; k++) {
+                sum += rowNeeded[k] * columnNeeded[k];
+            }
+            matrixC[i][j] = sum;
+        }
     }
 
     public static int[][] create(int size) {
